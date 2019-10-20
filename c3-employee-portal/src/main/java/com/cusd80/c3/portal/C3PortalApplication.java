52ed50ca569984eb86064125c1088cd07e49778c
Ty Garside <tyrone.garside@dcsg.com>
2019-10-20 12:07:26 -0400

    changes for heroku

---
+++ c3-employee-portal/src/main/java/com/cusd80/c3/portal/C3PortalApplication.java
@@ -0,0 +1,15 @@
+package com.cusd80.c3.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAutoConfiguration
+@SpringBootApplication
+public class C3PortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(C3PortalApplication.class, args);
+    }
+
+}
