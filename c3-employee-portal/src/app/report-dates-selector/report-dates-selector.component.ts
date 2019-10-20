cbdb8ef1baeded2201185ce3972856550f86fa17
dks0527583 <William.Kammermeier@dcsg.com>
2019-10-20 06:56:33 -0400

    Change report service selector to only select one service
    Changed report service selector to only select one service

--- c3-employee-portal/src/app/report-dates-selector/report-dates-selector.component.ts
+++ c3-employee-portal/src/app/report-dates-selector/report-dates-selector.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Service } from 'src/models/service';
 
 @Component({
   selector: 'app-report-dates-selector',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./report-dates-selector.component.scss']
 })
 export class ReportDatesSelectorComponent implements OnInit {
-
+  selectedService;
   constructor() { }
 
   ngOnInit() {
+    this.selectedService = JSON.parse(sessionStorage.getItem('selectedReportService'));
+    console.log(this.selectedService);
   }
 
 }
