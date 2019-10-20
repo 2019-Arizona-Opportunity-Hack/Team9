bb33a09ed941a0e1bf795b96e6a0e7b73664cc3f
Ty Garside <tyrone.garside@dcsg.com>
2019-10-20 01:49:02 -0400

    started family controller

--- c3-server/src/main/java/com/cusd80/c3/server/controller/FamilyController.java
+++ c3-server/src/main/java/com/cusd80/c3/server/controller/FamilyController.java
@@ -1,20 +1,42 @@
 package com.cusd80.c3.server.controller;
 
-import com.cusd80.c3.api.FamilyApi;
-import com.cusd80.c3.api.model.Caregiver;
-import com.cusd80.c3.api.model.Family;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cusd80.c3.api.FamilyApi;
+import com.cusd80.c3.api.model.Caregiver;
+import com.cusd80.c3.api.model.Caregiver.ChildCareTypeEnum;
+import com.cusd80.c3.api.model.Dependent;
+import com.cusd80.c3.api.model.Family;
+import com.cusd80.c3.server.entity.MemberEntity;
+import com.cusd80.c3.server.repo.MemberRepository;
 
 @RestController
 public class FamilyController implements FamilyApi {
 
+    @Autowired
+    private MemberRepository memberRepository;
+
     @Override
     public ResponseEntity<Family> familyCaregiverIdGet(String caregiverId) {
-        Family family = new Family();
-        Caregiver giver = new Caregiver();
-        giver.setCaregiverId("123");
-        family.setCaregiver(giver);
-        return ResponseEntity.ok(family);
+        return ResponseEntity.ok(mapFamily(memberRepository.findById(caregiverId).orElseThrow()));
     }
+
+    private Family mapFamily(MemberEntity member) {
+        Caregiver caregiver = new Caregiver();
+        caregiver.setCaregiverId(member.getId());
+        caregiver.setChildCareType(ChildCareTypeEnum.fromValue(member.getChildCareType()));
+
+        List<Dependent> dependents = new ArrayList<>();
+
+        Family family = new Family();
+        family.setCaregiver(caregiver);
+        family.setDependents(dependents);
+        return family;
+    }
+
 }
