b081be3400940df0119783fb44cb497f24311b8a
Jesse Coddington <jessecoddington@hotmail.com>
2019-10-20 04:20:11 -0700

    Add dependents and adjust styles

--- c3-employee-portal/src/app/addmember/addmember.component.spec.ts
+++ c3-employee-portal/src/app/addmember/addmember.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Member } from '../../models/member';
 import { AddmemberComponent } from './addmember.component';
 
 describe('AddmemberComponent', () => {
