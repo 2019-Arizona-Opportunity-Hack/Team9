d524cd07e1d7a0cf8cd0a4edea382c37bbb62558
Ty Garside <tyrone.garside@dcsg.com>
2019-10-20 02:10:32 -0400

    services entity mapping to api

--- c3-server/src/main/java/com/cusd80/c3/server/controller/ServiceController.java
+++ c3-server/src/main/java/com/cusd80/c3/server/controller/ServiceController.java
@@ -5,15 +5,11 @@ import javax.ws.rs.NotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cusd80.c3.api.ServiceApi;
 import com.cusd80.c3.api.model.Service;
-import com.cusd80.c3.server.entity.ServiceEntity;
 import com.cusd80.c3.server.mapper.ServiceMapper;
 import com.cusd80.c3.server.repo.ServiceRepository;
 
@@ -38,34 +34,6 @@ public class ServiceController implements ServiceApi {
         if (!serviceRegistry.existsById(serviceId)) throw new NotFoundException();
         serviceRegistry.deleteById(serviceId);
         return ResponseEntity.ok().build();
-    }
-
-    @RequestMapping(method = RequestMethod.GET)
-    public Iterable<ServiceEntity> list() {
-        return serviceRegistry.findAll();
-    }
-
-    @RequestMapping(path = "{id}", method = RequestMethod.GET)
-    public ServiceEntity get(@PathVariable("id") String id) {
-        return serviceRegistry.findById(id).orElse(null);
-    }
-
-    @RequestMapping(path = "{id}", method = RequestMethod.PUT)
-    public ServiceEntity put(@PathVariable(name = "id", required = false) String id, @RequestBody ServiceEntity service) {
-        if (id != null && !id.isBlank()) {
-            service.setId(id);
-        }
-        return serviceRegistry.save(service);
-    }
-
-    @RequestMapping(path = "{id}", method = RequestMethod.DELETE)
-    public boolean put(@PathVariable(name = "id", required = true) String id) {
-        if (serviceRegistry.existsById(id)) {
-            serviceRegistry.deleteById(id);
-            return true;
-        } else {
-            return false;
-        }
     }
 
 }
--- c3-server/src/main/java/com/cusd80/c3/server/controller/ServicesController.java
+++ c3-server/src/main/java/com/cusd80/c3/server/controller/ServicesController.java
@@ -1,11 +1,16 @@
 package com.cusd80.c3.server.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cusd80.c3.api.ServicesApi;
+import com.cusd80.c3.api.model.Service;
+import com.cusd80.c3.server.mapper.ServiceMapper;
 import com.cusd80.c3.server.repo.ServiceRepository;
 
 @RestController
@@ -15,10 +20,14 @@ public class ServicesController implements ServicesApi {
     @Autowired
     private ServiceRepository serviceRegistry;
 
+    @Autowired
+    private ServiceMapper serviceMapper;
+
     @Override
-    public ResponseEntity<Void> servicesGet() {
-        // TODO Auto-generated method stub
-        return ResponseEntity.ok().build();
+    public ResponseEntity<List<Service>> servicesGet() {
+        return ResponseEntity.ok(
+            serviceRegistry.findAllEnabledEqualsTrue().map(serviceMapper::fromEntity).collect(Collectors.toList())
+        );
     }
 
 }
