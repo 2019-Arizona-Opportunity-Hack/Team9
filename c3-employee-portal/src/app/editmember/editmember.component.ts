526d87d747d4542a247d64bbb6a9532a2d468b4b
Jesse Coddington <jessecoddington@hotmail.com>
2019-10-20 12:48:22 -0700

    Adjust member search

--- c3-employee-portal/src/app/editmember/editmember.component.ts
+++ c3-employee-portal/src/app/editmember/editmember.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-editmember',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditmemberComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  public onSubmit() {
+    this.router.navigate(['success']);
   }
-
 }
